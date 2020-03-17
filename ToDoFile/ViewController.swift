a778b0c9285b3369340c04999477d7e9ec13e128
Valentina Kalaida <valen2302@ukr.net>
2020-03-17 16:43:21 +0200

    Initial Commit

---
+++ ToDoFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoFile
+//
+//  Created by Валентина Калайда on 17.03.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
